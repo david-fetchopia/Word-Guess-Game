7474c8ba0cc4b24d2fcf1b3dd3dab54a8c042c71
DMa <davidweibinma@Weibins-MacBook-Pro.local>
2019-01-30 01:15:37 -0500

    added javascript code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,23 +2,59 @@ var numWins = 0;
 var numGuesses = 12;
 var randomWord = "";
 var possibleWords = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Nine"];
-var foundPosition = [];
+var foundLetters = [];
 //randomWord = possibleWords[(Math.floor(Math.random() * possibleWords.length))];
 //document.getElementById("currentPlanet").innerHTML = randomWord;
 function getPrintWord() {
     var randomWord = possibleWords[(Math.floor(Math.random() * possibleWords.length))];
     document.getElementById("currentPlanet").innerHTML = randomWord;
-    for(var i=0;i<randomWord.length;i++) {
+    for (var i = 0; i < randomWord.length; i++) {
         document.getElementById("currentPlanet").innerHTML += '<element id="letter' + i + '">';
         document.getElementById("letter" + i).innerHTML = "_ ";
     }
     return randomWord;
 }
-randomWord = getPrintWord();
+randomWord = getPrintWord().toLowerCase();
 document.onkeydown = function (e) {
-    document.getElementById("currentPlanet").innerHTML = randomWord;
-    document.getElementById("soFar").innerHTML += e.key + ", ";
+    var currentKey = e.key;
+    if (foundLetters.indexOf(currentKey) !== -1) {
+        //do nothing because its already a guessed letter
+    }
+    else if (randomWord.indexOf(currentKey) !== -1) {
+        //match letter case
+        document.getElementById("soFar").innerHTML += currentKey + ", ";
+        //update foundLetters
+        for (var i = 0; i < randomWord.length; i++) {
+            //loop through the word and detect for recurring matches of that letter
+            if (randomWord[i] === currentKey) {
+                //replacing "_" with found letter
+                document.getElementById("letter" + i).innerHTML = currentKey + " ";
+                foundLetters.push(currentKey);
+                //win condition
+                if (randomWord.length === foundLetters.length) {
+                    //reset game
+                    numWins++;
+                    numGuesses = 12;
+                    foundLetters = [];
+                    randomWord = getPrintWord().toLowerCase();
+                    document.getElementById("soFar").innerHTML = "";
+                    document.getElementById("numWins").innerHTML = numWins;
+                    document.getElementById("numGuesses").innerHTML = numGuesses;
+                    
 
-    document.getElementById("log").innerHTML = randomWord.indexOf(e.key);
+                    
+                    console.log("EVERYTHING FOUND");
+                }
+            }
+        }
+        console.log(foundLetters);
+    }
+    else {
+        //not a match case
+        numGuesses--;
+        document.getElementById("soFar").innerHTML += currentKey + ", ";
+        document.getElementById("numGuesses").innerHTML = numGuesses;
+    }
+
 
 }
